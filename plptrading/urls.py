0291677f056ebc009e51fe35a0d5a3b47dc660ce
Godfrey-Mbugua <godfreymbugua7@gmail.com>
2024-06-18 09:09:34 +0300

    Initial commit

--- plptrading/urls.py
+++ plptrading/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name ='plptrading'
 
@@ -10,3 +12,5 @@ urlpatterns = [
     path('customer_list/',views.customer_list,name ='customer_list'),
     path('customer/<int:id>/',views.customer_detail,name ='customer_detail'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
